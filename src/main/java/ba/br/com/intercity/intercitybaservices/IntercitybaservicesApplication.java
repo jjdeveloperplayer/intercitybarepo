46c8b1e57ff8112e5c311c48775a7d7ae76a25f2
jjdeveloperplayer <50576271+jjdeveloperplayer@users.noreply.github.com>
2019-05-14 21:10:04 -0300

    Add files via upload

---
+++ src/main/java/ba/br/com/intercity/intercitybaservices/IntercitybaservicesApplication.java
@@ -0,0 +1,13 @@
+package ba.br.com.intercity.intercitybaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercitybaservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercitybaservicesApplication.class, args);
+	}
+
+}
