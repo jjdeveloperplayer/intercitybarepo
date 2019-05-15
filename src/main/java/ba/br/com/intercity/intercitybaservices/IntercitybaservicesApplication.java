15a93cef5c6b60853d0deab1242b0e0ed0057309
jjdeveloperplayer <50576271+jjdeveloperplayer@users.noreply.github.com>
2019-05-15 18:10:20 -0300

    Add files via upload

--- src/main/java/ba/br/com/intercity/intercitybaservices/IntercitybaservicesApplication.java
+++ src/main/java/ba/br/com/intercity/intercitybaservices/IntercitybaservicesApplication.java
@@ -3,9 +3,11 @@ package ba.br.com.intercity.intercitybaservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableCaching
+@PropertySource("classpath:application.properties")
 public class IntercitybaservicesApplication {
 
 	public static void main(String[] args) {
